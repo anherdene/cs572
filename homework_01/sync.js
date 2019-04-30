0a7c2a35d3279320d4cee5eb6c6c82165cd7b3d0
anherdene <anherdene@gmail.com>
2019-04-29 21:20:17 -0500

    sync

---
+++ homework_01/sync.js
@@ -0,0 +1,16 @@
+
+const replace = function(source,filterwords) {
+    let newsource =  source.map(w => filterwords.includes(w) ? "***" : w);
+    return newsource;
+}
+
+String.prototype.filterwords = async function ( filterwords) {
+    let words = this.split(' ');
+
+   let result =  await replace(words,filterwords);
+    return result.join(" ");
+}
+
+
+
+console.log("This house is nice".filterwords(["house","nice"]));
