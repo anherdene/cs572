d00fd8600c4c00c58fb46b627d468ae620d13299
anherdene <anherdene@gmail.com>
2019-05-01 22:08:26 -0500

    homework 03 dns, promise

--- homework_03/dns.js
+++ homework_03/dns.js
@@ -1,5 +1,5 @@
 const dns = require('dns');
 dns.resolve4('www.mum.edu', (err, addresses) => {
-    if (err) throw err;
+    if (err) console.log(err);
     console.log(`addresses: ${JSON.stringify(addresses)}`);
 });
