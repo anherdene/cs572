d3a9026989be884bd835de68f346ed9a1f7f1872
anherdene <anherdene@gmail.com>
2019-05-03 21:38:20 -0500

    homework 05 express

--- homework_05/app.js
+++ homework_05/app.js
@@ -2,24 +2,24 @@ const express = require('express');
 const axios = require("axios");
 var app = express();
 const port =3000;
-
+const proxyUri = "https://randomuser.me/api/?results=10";
 app.listen(port, () => console.log("listening on "+port));
 app.enable('trust proxy');
 app.enable('strict routing');
 app.enable('case sensetive routing');
 app.disable('x-powered-by');
+app.set({
+    "Cache-control": "private, max-age = 86400",
+    "Last-modifier": new Date(),
+    "Link": `<{proxyUri}&page=1>; rel = "first",
+        <{proxyUri}&page=2>; rel = "next"`
+});
 app.route('/users')
     .get(async (req, res) => {
     console.log(`processing...`)
     try {
-        let result = await axios.get("https://randomuser.me/api/?results=10");
+        let result = await axios.get(proxyUri);
         console.log(result.data);
-        app.set({
-            "Cache-control": "private, max-age = 86400",
-            "Last-modifier": new Date(),
-            "Link": `<https://randomuser.me/api/?results=10&page=1>; rel = "first",
-        <https://randomuser.me/api/?results=10&page=2>; rel = "next"`
-        });
         res.json(result.data);
     } catch {
         res.send("error");
